82e3a0291cb4cfe574fa9cf2734c41ffb479b7bd
Mateo Vidal <matevidal@hotmail.com.ar>
2021-11-14 09:32:21 -0300

    probando nuevo posteos

--- src/screens/NewPostForm.js
+++ src/screens/NewPostForm.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Text, View, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
-
 import {db, auth} from "../firebase/config"
 
 
@@ -9,10 +8,9 @@ class NewPostForm extends Component {
     constructor(props){
         super(props)
         this.state = {
-            title: '',
-            description: '',
-
-        }
+            titulo: '',
+            descripcion: '',
+        };
     }
 
     // db.collection('posts').add({
@@ -41,66 +39,116 @@ class NewPostForm extends Component {
     //     }
     // )
 
-
-    render() {
-        return (
-            <View>
-                <TextInput
-                    placeholder='Título'
-                    keyboardType='default'
-                    onChangeText={ text => this.setState({
-                        title: text
-                    })}
-                    value={this.state.title}
-                    style={styles.input}
-                />
-                <TextInput
-                    placeholder='Descripción'
-                    keyboardType='default'
-                    onChangeText={ text => this.setState({
-                        description: text
-                    })}
-                    value={this.state.description}
-                    multiline={true}
-                    style={styles.input}
-                />
-                <TouchableOpacity style={styles.button} onPress={() => console.log('post')}>
-                    <Text style={styles.textButton}>
-                        Postear
-                    </Text>
-                </TouchableOpacity>
-            </View>
-        )
+    submitPost(){
+        db.collection("posteos").add({
+            user: auth.currentUser.email, 
+            descripcion: this.state.descripcion, 
+            titulo: this.state.titulo, 
+            createdAt: Date.now(), 
+            likes: [], 
+            comentarios: [],
+        })
+        .then(() => {
+            this.setState({
+                titulo:  "",
+                descripcion: "", 
+            })
+        })
+        .catch((err) => {
+           console.log(err)
+        })
     }
+
+//     render() {
+//         return (
+//             <View>
+//                 <TextInput
+//                     placeholder='Título'
+//                     keyboardType='default'
+//                     onChangeText={ text => this.setState({
+//                         title: text
+//                     })}
+//                     value={this.state.title}
+//                     style={styles.input}
+//                 />
+//                 <TextInput
+//                     placeholder='Descripción'
+//                     keyboardType='default'
+//                     onChangeText={ text => this.setState({
+//                         description: text
+//                     })}
+//                     value={this.state.description}
+//                     multiline={true}
+//                     style={styles.input}
+//                 />
+//                 <TouchableOpacity style={styles.button} onPress={() => console.log('post')}>
+//                     <Text style={styles.textButton}>
+//                         Postear
+//                     </Text>
+//                 </TouchableOpacity>
+//             </View>
+//         )
+//     }
+// }
+
+render() {
+    return (
+      <View style={styles.formContainer}>
+        <Text> Nuevo Post </Text>
+        <TextInput
+          onChangeText={(text) => this.setState({ titulo: text})}
+          placeholder="Titulo"
+          keyboardType="default"
+          value={this.state.titulo}
+          style={styles.multilineInput}
+        />
+        <TextInput
+          onChangeText={(text) => this.setState({ descripcion: text })}
+          placeholder="Descripción"
+          keyboardType="default"
+          value={this.state.descripcion}
+          multiline
+          style={styles.multilineInput}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => this.submitPost()}
+        >
+          <Text style={styles.textButton}> Postear </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    button: {
-        backgroundColor: "#28a745",
-        paddingHorizontal: 10,
-        paddingVertical: 6,
-        textAlign: "center",
-        borderRadius: 4,
-        borderWidth: 1,
-        borderStyle: "solid",
-        borderColor: "#28a745",
-        marginTop: 5,
-      },
-      textButton: {
-        color: "#fff",
-      },
-    input:{
-        height:20,
+    formContainer:{
+        paddingHorizontal:10,
+        marginTop: 20,
+    },
+    multilineInput:{
+        height:100,
         paddingVertical:15,
         paddingHorizontal: 10,
         borderWidth:1,
+        borderColor: '#ccc',
         borderStyle: 'solid',
+        borderRadius: 6,
         marginVertical:10,
-        margin: 5,
-        borderColor: "black",
-        borderWidth: "thin",
-        borderRadius: 5,
-      },
-})
+    },
+    button:{
+        backgroundColor:'#28a745',
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        textAlign: 'center',
+        borderRadius:4, 
+        borderWidth:1,
+        borderStyle: 'solid',
+        borderColor: '#28a745'
+    },
+    textButton:{
+        color: '#fff'
+    }
+});
 
 export default NewPostForm;
