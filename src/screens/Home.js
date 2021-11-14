920975dce86936b9b307e5aaa07d8c156397b40c
Mateo Vidal <matevidal@hotmail.com.ar>
2021-11-14 10:16:47 -0300

    home a medias, y arreglo de posteo

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,23 +1,74 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, {Component} from 'react';
+import { db, auth } from '../firebase/config'
+import {View, Text, TouchableOpacity, StyleSheet, FlatList, ActivityIndicator, Image} from 'react-native';
+import Post from '../components/Post'
 
- class Home extends Component {
-     constructor(props){
-        super(props);
-        this.state={
-
+class Home extends Component {
+    constructor(props){
+        super(props)
+        this.state = {   
+            posts: []
         }
     }
 
-    render() {
-        return (
-            <View>
-                <Text> Home </Text>
-                
-            </View>
-        )
+    componentDidMount() {
+        this.showPost();
     }
+
+    showPost() {
+        db.collection('posteos')
+        // .where('user','==', auth.currentUser.email)
+        .orderBy('createdAt', 'desc')
+        // .limit(2)
+        .onSnapshot((docs) => {
+            let posteos = []
+            docs.forEach((doc) => {
+                posteos.push({
+                    id: doc.id,
+                    data: doc.data()
+                })
+            })
+            this.setState({
+                posts: posteos
+            })
+        })
+    }
+
+    
+    render(){
+        return(
+            <View style={styles.container}>
+                <FlatList 
+                    data={this.state.posts}
+                    keyExtractor={(post) => post.id}
+                    // renderItem={({item}) => <Post postData={item} />}
+                />
+            </View>
+        )            
+    }
+
 }
 
+const styles = StyleSheet.create({
+    container: {
+        textAlign: 'center',
+        padding: 10,
+    },
+    image:{
+        height:250,
+    },
+    flatList:{
+        justifyContent:'space-between'
+    },
+    touchable:{
+        padding: 4,
+        backgroundColor: '#ccc',
+        marginBottom: 10,
+        borderRadius: 4,
+    },
+    touchableText:{
+        fontWeight: 'bold'
+    }
+});
 
 export default Home;
